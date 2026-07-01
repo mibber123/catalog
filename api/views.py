0a95ff2f4568f266753562ed5b439ea0c5f2ab7d
mibber123 <benjirocher@gmail.com>
2026-07-01 20:07:08 +0100

    update1

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# view for /api/book
+class bookview(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({'hi': 'django'})
+        #return render(request, 'book.html')
+
+book_view = bookview.as_view()
