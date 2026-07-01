2cbf1b2c2d02e88335a0aaef72fb4483ac737665
ben <97813842+mibber123@users.noreply.github.com>
2026-07-01 21:06:31 +0100

    update3

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('books/', views.book_view, name='book_view'),
-    #re_path(r'books/$', views.book_view, name='books'),
+    #path('books/$', views.book_view, name='book_view'),
+    re_path(r'books/$', views.book_view, name='books'),
     #re_path(r'^books/(?P<id>\d+)/$', views.book_detail, name='book_detail'),
 ]
